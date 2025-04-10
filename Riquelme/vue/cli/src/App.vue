934291aef92bbd814bc7a1174255336a359e51fe
Riquelme Braga <72748812+riquelme00@users.noreply.github.com>
2025-04-09 21:49:26 -0300

    criando home component

---
+++ Riquelme/vue/cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <home-component-vue/>
+  </div>
+</template>
+
+<script>
+import HomeComponentVue from './components/HomeComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponentVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
