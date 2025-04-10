43e1dac724bd02bb66a027201088692efbe67eda
Fernando Dias <fernando.dias@sicoob.com.br>
2025-04-09 21:45:35 -0300

    criando nossa HomeComponent

--- Professor/sand-box/vue/cli/src/App.vue
+++ Professor/sand-box/vue/cli/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>VUE</h1>
+    <br>
+    <home-component-vue/>
+    <br>
+    <home-component-vue/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponentVue from './components/HomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeComponentVue
   }
 }
 </script>
