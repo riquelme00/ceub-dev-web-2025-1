162b416ee0b416237fa7d7def9c0a32c2563ba35
Riquelme Braga <72748812+riquelme00@users.noreply.github.com>
2025-04-23 21:56:15 -0300

    aula de diretivas do vue

--- Riquelme/sand-box/vue/cli/src/App.vue
+++ Riquelme/sand-box/vue/cli/src/App.vue
@@ -7,18 +7,25 @@
     <home-component/>
     <br>
     <usuario-component/>
+    <br>
+    <imagem-component/>
+    <br>
+    <estilo-component/>
   </div>
 </template>
 
 <script>
 import HomeComponent from './components/HomeComponent.vue'
+import ImagemComponent from './components/ImagemComponent.vue';
 import UsuarioComponent from './components/UsuarioComponent.vue';
-
+import EstiloComponent from './components/EstiloComponent.vue';
 export default {
   name: 'App',
   components: {
     HomeComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    ImagemComponent,
+    EstiloComponent
   }
   , 
   created() {
---
+++ Riquelme/sand-box/vue/cli/src/components/ImagemComponent.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+        <img v-show="imagemVisivel" :src="pathImagem" alt="Banner">
+        <button @click="apareceEsconnde(), segundoMetodo()">Aparece/Esconde</button>
+    </div>
+</template>
+
+<script>
+
+
+   export default {
+    name: "ImagemComponent",
+    data(){
+        return{
+            imagemVisivel : true, 
+            banner:"valor do alt",
+            pathImagem : "https://www.ogol.com.br/img/jogadores/new/10/22/1022_ronaldo_20241012221007.jpg"
+        }
+    },
+        methods:{
+            apareceEsconnde() {
+                this.imagemVisivel = !this.imagemVisivel;
+            },
+            segundoMetodo(){
+                setTimeout(() => {
+                    alert("Segundo metodo acionado");
+                }, 2000);
+            }
+        }
+   }
+</script>
+
+<style>
+
+    img {
+        width: 300px;
+        height: auto;
+        border: solid 4px darkcyan;
+        margin: 16px;
+        border-radius: 16px;
+        box-shadow: 2px 2px 10px 2px darkcyan;
+    }
+
+    button{
+        padding: 12px;
+        font-weight: bold;
+        color: darkcyan;
+        background: azure;
+        border: solid 2px darkcyan;
+        border-radius:  8px; 
+        cursor: pointer;
+        transition: 0.5s;
+
+    }
+    button:hover{
+        background-color: darkcyan;
+        color:azure
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        align-items: center;
+        margin: 16px;
+        border-bottom: solid 2px lightblue;
+        padding: 16px;
+
+    }
+
+</style>
