89f339de8644df0173027ba04bb8d9af8e5264b1
Fernando Dias <fernando.dias@sicoob.com.br>
2025-06-25 21:01:36 -0300

    finalizando projeto t-burguer

--- Professor/tburguer/src/views/PedidosView.vue
+++ Professor/tburguer/src/views/PedidosView.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
         <h1>Pedido</h1>
+        <lista-pedido-component/>
+    
     </div>
 </template>
 
 <script>
+
+    import ListaPedidoComponent from '@/components/ListaPedidoComponent.vue';
  
     export default {
-        name : "PedidosView"
+        name : "PedidosView",
+        components: {
+            ListaPedidoComponent
+        }
     }
 </script>
 
